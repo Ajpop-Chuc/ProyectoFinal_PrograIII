33f4235f6e25223b30165cdcdd8f67e7e394a700
Ajpop Chuc <ajpopchuc2004@gmail.com>
2022-06-11 19:20:50 -0600

    Rsolucion Conflicto master - Ivana

--- ReporteConsultas.aspx.cs
+++ ReporteConsultas.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,46 @@ namespace ProyectoFinalP_PrograIII
 {
     public partial class ReporteConsultas : System.Web.UI.Page
     {
+        
+
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnVer_Click(object sender, EventArgs e)
+        {
+
+           
+
+            if (RBEnfermedades.Checked)
+            {
+               ControlDiagnostico.leerJson(Server.MapPath("ControlEnfermedades.json"));
+                GridViewReporteC.Visible = true;
+                GridViewReporteC.DataSource = ControlDiagnostico.ListaControlEnfermedades; 
+            }
+
+            if (RBEdades.Checked)
+            {
+                LabelEdadPromedio.Visible = true;
+                Pacientes paciente = new Pacientes();
+                LabelEdadPromedio.Text = "La edad promedio de pacientes es: ";
+                    
+                
+
+            }
+
+            if (RBIngresos.Checked)
+            {
+
+            }
+
+            if (RBMedicamentos.Checked)
+            {
+                ControlMedicamento.leerJson(Server.MapPath("ControlMedicamentos.json"));
+                GridViewReporteC.Visible = true;
+                GridViewReporteC.DataSource = ControlMedicamento.ListaControlMedicamentos;
+            }
         }
     }
 }
